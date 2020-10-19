be52585f896308def3c9b78049b513f3b4351b61
listguy <nitzanjl@gmail.com>
2020-10-19 12:58:05 +0300

    fixed server (i hope)

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3001;
 const path = require("path");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client", "build")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 //Sends scores in groups of 40, starting from offset
