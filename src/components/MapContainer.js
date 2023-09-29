2574236a5287adc5655f7d0a840adc1a7dd74740
listguy <nitzanjl@gmail.com>
2023-09-29 15:57:26 +0300

    updated map loading in client

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 import mapStyles from "./mapStyles.json";
 import greenPin from "../images/green-pointer.png";
 const { REACT_APP_GOOGLE_API_KEY } = process.env;
 
-export default function MapContainer({
-  currObjective,
-  userPick,
-  isBreak,
-  setBreak,
-  pinMarker,
-}) {
+export default function MapContainer({ currObjective, userPick, isBreak, setBreak, pinMarker }) {
   const mapContainerStyles = {
     width: "98.8vw",
     height: "100vh",
@@ -29,33 +23,32 @@ export default function MapContainer({
     pinMarker(location);
   };
 
-  return (
-    <>
-      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
-        <GoogleMap
-          mapContainerStyle={mapContainerStyles}
-          zoom={8}
-          center={center}
-          options={{
-            disableDefaultUI: true,
-            styles: mapStyles, //removes locations name
-            streetViewControl: false,
-            fullscreenControl: false,
-            gestureHandling: "cooperative",
-          }}
-          onClick={makeMove}
-        >
-          {userPick && <Marker key={"user"} position={userPick.location} />}
-          {isBreak && (
-            <Marker
-              key={"currObjective"}
-              position={currObjective.location}
-              color="green"
-              options={{ icon: greenPin }}
-            />
-          )}
-        </GoogleMap>
-      </LoadScript>
-    </>
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: REACT_APP_GOOGLE_API_KEY,
+  });
+
+  console.log(process.env.REACT_APP_GOOGLE_API_KEY);
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={mapContainerStyles}
+      zoom={8}
+      center={center}
+      options={{
+        disableDefaultUI: true,
+        styles: mapStyles, //removes locations name
+        streetViewControl: false,
+        fullscreenControl: false,
+        gestureHandling: "cooperative",
+      }}
+      onClick={makeMove}
+    >
+      {userPick && <Marker key={"user"} position={userPick.location} />}
+      {isBreak && (
+        <Marker key={"currObjective"} position={currObjective.location} color="green" options={{ icon: greenPin }} />
+      )}
+    </GoogleMap>
+  ) : (
+    <></>
   );
 }
